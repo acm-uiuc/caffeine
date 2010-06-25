7bc95e9d5c35f3a96902d49c747abb83d8b397c5
Mike Crawford <mike@tuxnami.org>
2010-06-25 14:07:00 -0500

    2.0 - Now functions as toggle requried for 0.4 and up.

--- 5-18_Decoder/5-18_Decoder.asm
+++ 5-18_Decoder/5-18_Decoder.asm
@@ -44,8 +44,9 @@
 ; Now that that's all done...
 ;
 ; Input pins are PD[0:4]
-; If all inputs are 0 or above 18 the outputs are 0.  Otherwise
-; the selected output is 1 and the remaining are zero.
+; If all inputs are 0 or above 18 the outputs are previous value.  Otherwise
+; the selected output is toggled and the remaining values are their previous
+; value.  29 clears all, 30 clears LCD pins, 31 sets LCD pins.
 ; Outputs are assigned from PC[6] rotating counterclockwise
 ; to PB[6].
 ;
@@ -53,6 +54,10 @@
 
 ; Revision history:
 ;
+; v2.0
+; - Changed behavior to toggle instead of only being on when the
+;   input is at the value, bugfix for LCD enable lines.
+;   - mike <mike@tuxnami.org>
 ; v1.1
 ;	- Added a condition that if the inputs are 11111b the LCD enable
 ;	  lines will be active.
@@ -90,6 +95,7 @@ RESET:
 
 		LDI r20, $1F		; r20 - Input mask
 		LDI r21, $40		; r21 - offset for jumping
+		LDI r22, $00		; r22 - Toggle mask
 		LDI r28, $00		; r28 - store previous value of input lines
 		LDI r31, $00		; r31 - Top half of Z register, must be zero for IJMP
 
@@ -103,83 +109,120 @@ poll_loop:					; main program loop
 		LSL r30				; multiply by 2
 		ADD r30, r21		; Add offset to r30
 
-		OUT PORTB, r16		; Clear the output ports
-		OUT PORTC, r16
-		OUT PORTD, r16
+		LDI r22, $00		; clear the toggle mask
 		IJMP				; Perform the dreaded IJMP
 
 .org $0040
 		RJMP poll_loop		; If input is zero all outputs should be off
 
 .org $0042
-		SBI PORTC, 6		; Raise outputs based on where the IJMP jumps
+		LDI r22, $40		; 1
+		RJMP toggle_c
+		LDI r22, $20		; 2
+		RJMP toggle_c
+		LDI r22, $10		; 3
+		RJMP toggle_c
+		LDI r22, $08		; 4
+		RJMP toggle_c
+		LDI r22, $04		; 5
+		RJMP toggle_c
+		LDI r22, $02		; 6
+		RJMP toggle_c
+		LDI r22, $01		; 7
+		RJMP toggle_c
+		LDI r22, $20		; 8
+		RJMP toggle_b
+		LDI r22, $10		; 9
+		RJMP toggle_b
+		LDI r22, $08		; 10
+		RJMP toggle_b
+		LDI r22, $04		; 11
+		RJMP toggle_b
+		LDI r22, $02		; 12
+		RJMP toggle_b
+		LDI r22, $01		; 13
+		RJMP toggle_b
+		LDI r22, $80		; 14
+		RJMP toggle_d
+		LDI r22, $40		; 15
+		RJMP toggle_d
+		LDI r22, $20		; 16
+		RJMP toggle_d
+		LDI r22, $40		; 17
+		RJMP toggle_b
+		LDI r22, $80		; 18
+		RJMP toggle_b
+		NOP							; 19
 		RJMP poll_loop
-		SBI PORTC, 5
+		NOP							; 20
 		RJMP poll_loop
-		SBI PORTC, 4
+		NOP							; 21
 		RJMP poll_loop
-		SBI PORTC, 3
+		NOP							; 22
 		RJMP poll_loop
-		SBI PORTC, 2
+		NOP							; 23
 		RJMP poll_loop
-		SBI PORTC, 1
+		NOP							; 24
 		RJMP poll_loop
-		SBI PORTC, 0
+		NOP							; 25
 		RJMP poll_loop
-		SBI PORTB, 5
+		NOP							; 26
 		RJMP poll_loop
-		SBI PORTB, 4
+		NOP							; 27
 		RJMP poll_loop
-		SBI PORTB, 3
+		NOP							; 28
 		RJMP poll_loop
-		SBI PORTB, 2
-		RJMP poll_loop
-		SBI PORTB, 1
-		RJMP poll_loop
-		SBI PORTB, 0
-		RJMP poll_loop
-		SBI PORTD, 7
-		RJMP poll_loop
-		SBI PORTD, 6
-		RJMP poll_loop
-		SBI PORTD, 5
-		RJMP poll_loop
-		SBI PORTB, 6
-		RJMP poll_loop
-		SBI PORTB, 7
-		RJMP poll_loop
-		NOP					; NOP!!!!!!
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		SBRC r16, 0
-		RJMP lcd_enable_set
-		OUT PORTB, r16		; Oops, someone done something dum, like putting a
-		OUT PORTC, r16		; value > 18 on the input lines.  Clear outputs, 
-		OUT PORTD, r16		; return to sender.
-		RJMP poll_loop
-lcd_enable_set:
-		OUT PORTB, r0
-		OUT PORTC, r16
-		OUT PORTD, r1
+		NOP							; 29
+		RJMP clear_all
+		NOP							; 30
+		RJMP clear_lcd
+		NOP							; 31
+		RJMP set_lcd
+
+toggle_b:
+		IN r23, PINB
+		EOR r23, r22
+		OUT PORTB, r23
 		RJMP poll_loop
 
+toggle_c:
+		IN r23, PINC
+		EOR r23, r22
+		OUT PORTC, r23
+		RJMP poll_loop
+
+toggle_d:
+		IN r23, PIND
+		EOR r23, r22
+		OUT PORTD, r23
+		RJMP poll_loop
+
+clear_all:
+		OUT PORTB, r16
+		OUT PORTC, r16
+		OUT PORTD, r16
+		RJMP poll_loop
+
+clear_lcd:
+		LDI r22, $30
+		IN r23, PINB
+		AND r23, r22
+		OUT PORTB, r23
+
+		LDI r22, $1F
+		IN r23, PIND
+		AND r23, r22
+		OUT PORTD, r23
+		RJMP poll_loop
+
+set_lcd:
+		LDI r22, $CF
+		IN r23, PINB
+		OR r23, r22
+		OUT PORTB, r23
+
+		LDI r22, $EO
+		IN r23, PINB
+		OR r23, r22
+		OUT PORTB, r23
+		RJMP poll_loop
